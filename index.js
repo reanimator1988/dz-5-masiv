3581c4373a0244865174ef3c10dc18e533ce63d2
DiMa <francev.dima@gmail.com>
2023-05-31 13:16:10 +0200

    dz-5-masiv.git

---
+++ index.js
@@ -0,0 +1,57 @@
+
+const arr = [16,-37,54,-4,72,-56,47,4,-16,25,-37,46,4,-51,27,-63,4,-54,76,-4,12,-35,4,47]
+
+const number = 18; 
+const element = arr[number];
+
+let count = 0;
+let sum = 0;
+for (let i = 0; i < arr.length; i++) {
+    if (arr[i] > 0) { 
+        count++;                  
+        sum += arr[i];
+        }
+}
+
+let int = 0;
+for (let i = 0; i < arr.length; i++) {
+    if (arr[i] < 0) {
+        int++;
+    }
+}
+
+let num = 0;
+let sum1 = 0;
+for (let i = 0; i < arr.length; i++) {
+    if (arr[i] > 0 && arr[i] % 2 === 0) {
+        num++;
+        sum1 += arr[i];
+   }
+}
+
+let nplus  = 0;
+let nsum = 0;
+for (let i = 0; i < arr.length; i++) {
+    if (arr[i] > 0 && arr[i] % 2 !== 0) {
+        nplus++;
+        nsum += arr[i];
+   }
+}
+
+let result = 1;
+for (let i = 0; i < arr.length; i++) {
+    if (arr[i] > 0) {                   
+        result *= arr[i];
+        }
+}
+
+alert(`1) Кількість позитивних елементів - ${count}
+2) Cума позитивних елементів - ${sum}
+3) Mаксимальний елемент масиву - ${element} 
+    та його порядковий номер - ${number}
+4) Кількість негативних елементів - ${int}
+5) Кількість парних позитивних елементів - ${num}
+6) Cума парних позитивних елементів - ${sum1}
+7) Кількість непарних позитивних елементів - ${nplus}
+8) Cума непарних позитивних елементів - ${nsum}
+9) Добуток позитивних елементів - ${result}`);
