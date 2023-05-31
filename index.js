96d269d0d51a6704169131026633028b66460928
DiMa <francev.dima@gmail.com>
2023-05-31 20:33:58 +0200

    Перездача дз5

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 
 const arr = [16,-37,54,-4,72,-56,47,4,-16,25,-37,46,4,-51,27,-63,4,-54,76,-4,12,-35,4,47]
-
-const number = 18; 
-const element = arr[number];
+ 
+const element = arr[18];
 
 let count = 0;
 let sum = 0;
